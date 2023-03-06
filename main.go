ac17d04a9b7617a7a10f5b3ae09cb0bb27a4fa71
samuel1992 <dantasamuca@gmail.com>
2023-03-06 23:09:31 +0100

    add sample configuration and use spotify player

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"github.com/samuel1992/playlist_migrator/players"
+	"net/http"
+)
+
 func main() {
+	config := parseConfig()
+	player := players.Spotify{HttpClient: &http.Client{}, Config: config.Spotify}
+
+	fmt.Println("Playlists:")
+	fmt.Println(player.GetPlaylists())
 }
