6c925773be872b93439be05d473f9cbb00613d1b
samuel1992 <dantasamuca@gmail.com>
2023-03-10 18:04:32 +0100

    fetch playlist musics with go routines

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/samuel1992/playlist_migrator/players"
 	"net/http"
 	"os"
+	"sync"
+
+	"github.com/samuel1992/playlist_migrator/players"
 )
 
 func main() {
@@ -18,6 +20,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Playlists:")
-	fmt.Println(playlists)
+  wg := &sync.WaitGroup{} 
+  chMusics := make(chan []players.Music, len(playlists))
+
+  wg.Add(len(playlists))
+  for index, playlist := range playlists {
+    go getMusics(player, playlist, wg, chMusics)
+    playlists[index].Musics = <- chMusics
+  }
+  wg.Wait()
+
+  for _, playlist := range playlists {
+    fmt.Println("new playlist ----->")
+    fmt.Printf("%+v\n", playlist)
+  }
+
+  fmt.Println("FINISH")
 }
+
+ func getMusics(player *players.Spotify, playlist players.Playlist, wg *sync.WaitGroup, chMusics chan []players.Music){
+   musics, err := player.GetMusics(playlist.ID)
+   if err != nil {
+     fmt.Println(err)
+   }
+
+   chMusics <- musics
+   wg.Done()
+ } 
