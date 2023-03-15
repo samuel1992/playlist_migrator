50db4277452c88de5ed4330a470e4c3631aefc28
samuel1992 <dantasamuca@gmail.com>
2023-03-16 00:57:30 +0100

    change the types for another file

--- players/spotify.go
+++ players/spotify.go
@@ -8,14 +8,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
-	"time"
 )
-
-type authResponse struct {
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
-	ExpiresIn   int64  `json:"expires_in"`
-}
 
 type SpotifyConfig struct {
 	UserID       string `yaml:"user_id"`
@@ -27,172 +20,6 @@ type Spotify struct {
 	HttpClient HTTPClient
 	Config     SpotifyConfig
 	ApiKey     string
-}
-
-type SpotifyPlayListsResponse struct {
-	Href  string `json:"href"`
-	Items []struct {
-		Collaborative bool   `json:"collaborative"`
-		Description   string `json:"description"`
-		ExternalUrls  struct {
-			Spotify string `json:"spotify"`
-		} `json:"external_urls"`
-		Href   string `json:"href"`
-		ID     string `json:"id"`
-		Images []struct {
-			Height int    `json:"height"`
-			URL    string `json:"url"`
-			Width  int    `json:"width"`
-		} `json:"images"`
-		Name  string `json:"name"`
-		Owner struct {
-			DisplayName  string `json:"display_name"`
-			ExternalUrls struct {
-				Spotify string `json:"spotify"`
-			} `json:"external_urls"`
-			Href string `json:"href"`
-			ID   string `json:"id"`
-			Type string `json:"type"`
-			URI  string `json:"uri"`
-		} `json:"owner"`
-		PrimaryColor any    `json:"primary_color"`
-		Public       bool   `json:"public"`
-		SnapshotID   string `json:"snapshot_id"`
-		Tracks       struct {
-			Href  string `json:"href"`
-			Total int    `json:"total"`
-		} `json:"tracks"`
-		Type string `json:"type"`
-		URI  string `json:"uri"`
-	} `json:"items"`
-	Limit    int `json:"limit"`
-	Next     any `json:"next"`
-	Offset   int `json:"offset"`
-	Previous any `json:"previous"`
-	Total    int `json:"total"`
-}
-
-type SpotifyPlayListDetailResponse struct {
-	Collaborative bool   `json:"collaborative"`
-	Description   string `json:"description"`
-	ExternalUrls  struct {
-		Spotify string `json:"spotify"`
-	} `json:"external_urls"`
-	Followers struct {
-		Href  any `json:"href"`
-		Total int `json:"total"`
-	} `json:"followers"`
-	Href   string `json:"href"`
-	ID     string `json:"id"`
-	Images []struct {
-		Height int    `json:"height"`
-		URL    string `json:"url"`
-		Width  int    `json:"width"`
-	} `json:"images"`
-	Name  string `json:"name"`
-	Owner struct {
-		DisplayName  string `json:"display_name"`
-		ExternalUrls struct {
-			Spotify string `json:"spotify"`
-		} `json:"external_urls"`
-		Href string `json:"href"`
-		ID   string `json:"id"`
-		Type string `json:"type"`
-		URI  string `json:"uri"`
-	} `json:"owner"`
-	PrimaryColor any    `json:"primary_color"`
-	Public       bool   `json:"public"`
-	SnapshotID   string `json:"snapshot_id"`
-	Tracks       struct {
-		Href  string `json:"href"`
-		Items []struct {
-			AddedAt time.Time `json:"added_at"`
-			AddedBy struct {
-				ExternalUrls struct {
-					Spotify string `json:"spotify"`
-				} `json:"external_urls"`
-				Href string `json:"href"`
-				ID   string `json:"id"`
-				Type string `json:"type"`
-				URI  string `json:"uri"`
-			} `json:"added_by"`
-			IsLocal      bool `json:"is_local"`
-			PrimaryColor any  `json:"primary_color"`
-			Track        struct {
-				Album struct {
-					AlbumType string `json:"album_type"`
-					Artists   []struct {
-						ExternalUrls struct {
-							Spotify string `json:"spotify"`
-						} `json:"external_urls"`
-						Href string `json:"href"`
-						ID   string `json:"id"`
-						Name string `json:"name"`
-						Type string `json:"type"`
-						URI  string `json:"uri"`
-					} `json:"artists"`
-					AvailableMarkets []string `json:"available_markets"`
-					ExternalUrls     struct {
-						Spotify string `json:"spotify"`
-					} `json:"external_urls"`
-					Href   string `json:"href"`
-					ID     string `json:"id"`
-					Images []struct {
-						Height int    `json:"height"`
-						URL    string `json:"url"`
-						Width  int    `json:"width"`
-					} `json:"images"`
-					Name                 string `json:"name"`
-					ReleaseDate          string `json:"release_date"`
-					ReleaseDatePrecision string `json:"release_date_precision"`
-					TotalTracks          int    `json:"total_tracks"`
-					Type                 string `json:"type"`
-					URI                  string `json:"uri"`
-				} `json:"album"`
-				Artists []struct {
-					ExternalUrls struct {
-						Spotify string `json:"spotify"`
-					} `json:"external_urls"`
-					Href string `json:"href"`
-					ID   string `json:"id"`
-					Name string `json:"name"`
-					Type string `json:"type"`
-					URI  string `json:"uri"`
-				} `json:"artists"`
-				AvailableMarkets []string `json:"available_markets"`
-				DiscNumber       int      `json:"disc_number"`
-				DurationMs       int      `json:"duration_ms"`
-				Episode          bool     `json:"episode"`
-				Explicit         bool     `json:"explicit"`
-				ExternalIds      struct {
-					Isrc string `json:"isrc"`
-				} `json:"external_ids"`
-				ExternalUrls struct {
-					Spotify string `json:"spotify"`
-				} `json:"external_urls"`
-				Href        string `json:"href"`
-				ID          string `json:"id"`
-				IsLocal     bool   `json:"is_local"`
-				Name        string `json:"name"`
-				Popularity  int    `json:"popularity"`
-				PreviewURL  any    `json:"preview_url"`
-				Track       bool   `json:"track"`
-				TrackNumber int    `json:"track_number"`
-				Type        string `json:"type"`
-				URI         string `json:"uri"`
-			} `json:"track"`
-			VideoThumbnail struct {
-				URL any `json:"url"`
-			} `json:"video_thumbnail"`
-		} `json:"items"`
-		Limit    int `json:"limit"`
-		Next     any `json:"next"`
-		Offset   int `json:"offset"`
-		Previous any `json:"previous"`
-		Total    int `json:"total"`
-	} `json:"tracks"`
-	Type string `json:"type"`
-	URI  string `json:"uri"`
 }
 
 func (s *Spotify) FetchApiKey() {
@@ -217,7 +44,7 @@ func (s *Spotify) FetchApiKey() {
 		fmt.Println(err.Error())
 	}
 
-	jsonResponseData := &authResponse{}
+	jsonResponseData := &spotifyAuthResponse{}
 	err = json.Unmarshal(responseData, jsonResponseData)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -227,7 +54,7 @@ func (s *Spotify) FetchApiKey() {
 }
 
 func parsePlaylists(data []byte) []Playlist {
-	var playlistsResponse SpotifyPlayListsResponse
+	var playlistsResponse spotifyPlayListsResponse
 	json.Unmarshal([]byte(data), &playlistsResponse)
 
 	var playlists []Playlist
@@ -239,7 +66,7 @@ func parsePlaylists(data []byte) []Playlist {
 }
 
 func parsePlaylistMusics(data []byte) []Music {
-	var playListDetailResponse SpotifyPlayListDetailResponse
+	var playListDetailResponse spotifyPlayListDetailResponse
 	json.Unmarshal([]byte(data), &playListDetailResponse)
 
 	var musics []Music
---
+++ players/spotify_types.go
@@ -0,0 +1,199 @@
+package players
+
+import (
+	"net/http"
+	"time"
+)
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type Music struct {
+	ID   string
+	Name string
+}
+
+type Playlist struct {
+	ID     string
+	Name   string
+	Musics []Music
+}
+
+type Player interface {
+	FetchApiKey()
+	GetPlaylists() ([]Playlist, error)
+	GetMusics(playListId string) ([]Music, error)
+}
+
+type spotifyAuthResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
+
+type spotifyPlayListsResponse struct {
+	Href  string `json:"href"`
+	Items []struct {
+		Collaborative bool   `json:"collaborative"`
+		Description   string `json:"description"`
+		ExternalUrls  struct {
+			Spotify string `json:"spotify"`
+		} `json:"external_urls"`
+		Href   string `json:"href"`
+		ID     string `json:"id"`
+		Images []struct {
+			Height int    `json:"height"`
+			URL    string `json:"url"`
+			Width  int    `json:"width"`
+		} `json:"images"`
+		Name  string `json:"name"`
+		Owner struct {
+			DisplayName  string `json:"display_name"`
+			ExternalUrls struct {
+				Spotify string `json:"spotify"`
+			} `json:"external_urls"`
+			Href string `json:"href"`
+			ID   string `json:"id"`
+			Type string `json:"type"`
+			URI  string `json:"uri"`
+		} `json:"owner"`
+		PrimaryColor any    `json:"primary_color"`
+		Public       bool   `json:"public"`
+		SnapshotID   string `json:"snapshot_id"`
+		Tracks       struct {
+			Href  string `json:"href"`
+			Total int    `json:"total"`
+		} `json:"tracks"`
+		Type string `json:"type"`
+		URI  string `json:"uri"`
+	} `json:"items"`
+	Limit    int `json:"limit"`
+	Next     any `json:"next"`
+	Offset   int `json:"offset"`
+	Previous any `json:"previous"`
+	Total    int `json:"total"`
+}
+
+type spotifyPlayListDetailResponse struct {
+	Collaborative bool   `json:"collaborative"`
+	Description   string `json:"description"`
+	ExternalUrls  struct {
+		Spotify string `json:"spotify"`
+	} `json:"external_urls"`
+	Followers struct {
+		Href  any `json:"href"`
+		Total int `json:"total"`
+	} `json:"followers"`
+	Href   string `json:"href"`
+	ID     string `json:"id"`
+	Images []struct {
+		Height int    `json:"height"`
+		URL    string `json:"url"`
+		Width  int    `json:"width"`
+	} `json:"images"`
+	Name  string `json:"name"`
+	Owner struct {
+		DisplayName  string `json:"display_name"`
+		ExternalUrls struct {
+			Spotify string `json:"spotify"`
+		} `json:"external_urls"`
+		Href string `json:"href"`
+		ID   string `json:"id"`
+		Type string `json:"type"`
+		URI  string `json:"uri"`
+	} `json:"owner"`
+	PrimaryColor any    `json:"primary_color"`
+	Public       bool   `json:"public"`
+	SnapshotID   string `json:"snapshot_id"`
+	Tracks       struct {
+		Href  string `json:"href"`
+		Items []struct {
+			AddedAt time.Time `json:"added_at"`
+			AddedBy struct {
+				ExternalUrls struct {
+					Spotify string `json:"spotify"`
+				} `json:"external_urls"`
+				Href string `json:"href"`
+				ID   string `json:"id"`
+				Type string `json:"type"`
+				URI  string `json:"uri"`
+			} `json:"added_by"`
+			IsLocal      bool `json:"is_local"`
+			PrimaryColor any  `json:"primary_color"`
+			Track        struct {
+				Album struct {
+					AlbumType string `json:"album_type"`
+					Artists   []struct {
+						ExternalUrls struct {
+							Spotify string `json:"spotify"`
+						} `json:"external_urls"`
+						Href string `json:"href"`
+						ID   string `json:"id"`
+						Name string `json:"name"`
+						Type string `json:"type"`
+						URI  string `json:"uri"`
+					} `json:"artists"`
+					AvailableMarkets []string `json:"available_markets"`
+					ExternalUrls     struct {
+						Spotify string `json:"spotify"`
+					} `json:"external_urls"`
+					Href   string `json:"href"`
+					ID     string `json:"id"`
+					Images []struct {
+						Height int    `json:"height"`
+						URL    string `json:"url"`
+						Width  int    `json:"width"`
+					} `json:"images"`
+					Name                 string `json:"name"`
+					ReleaseDate          string `json:"release_date"`
+					ReleaseDatePrecision string `json:"release_date_precision"`
+					TotalTracks          int    `json:"total_tracks"`
+					Type                 string `json:"type"`
+					URI                  string `json:"uri"`
+				} `json:"album"`
+				Artists []struct {
+					ExternalUrls struct {
+						Spotify string `json:"spotify"`
+					} `json:"external_urls"`
+					Href string `json:"href"`
+					ID   string `json:"id"`
+					Name string `json:"name"`
+					Type string `json:"type"`
+					URI  string `json:"uri"`
+				} `json:"artists"`
+				AvailableMarkets []string `json:"available_markets"`
+				DiscNumber       int      `json:"disc_number"`
+				DurationMs       int      `json:"duration_ms"`
+				Episode          bool     `json:"episode"`
+				Explicit         bool     `json:"explicit"`
+				ExternalIds      struct {
+					Isrc string `json:"isrc"`
+				} `json:"external_ids"`
+				ExternalUrls struct {
+					Spotify string `json:"spotify"`
+				} `json:"external_urls"`
+				Href        string `json:"href"`
+				ID          string `json:"id"`
+				IsLocal     bool   `json:"is_local"`
+				Name        string `json:"name"`
+				Popularity  int    `json:"popularity"`
+				PreviewURL  any    `json:"preview_url"`
+				Track       bool   `json:"track"`
+				TrackNumber int    `json:"track_number"`
+				Type        string `json:"type"`
+				URI         string `json:"uri"`
+			} `json:"track"`
+			VideoThumbnail struct {
+				URL any `json:"url"`
+			} `json:"video_thumbnail"`
+		} `json:"items"`
+		Limit    int `json:"limit"`
+		Next     any `json:"next"`
+		Offset   int `json:"offset"`
+		Previous any `json:"previous"`
+		Total    int `json:"total"`
+	} `json:"tracks"`
+	Type string `json:"type"`
+	URI  string `json:"uri"`
+}
