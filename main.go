318565418a42b00b141f8e6dd1746a15e1c89e9f
samuel1992 <dantasamuca@gmail.com>
2023-03-27 14:12:57 +0200

    add get playlists command

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/samuel1992/playlist_migrator/players"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"sync"
 )
 
+type PlayersConfig struct {
+	Spotify players.SpotifyConfig `yaml:"spotify"`
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("You have to specify an available command:\n - get-playlists")
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "get-playlists":
+		getPlaylists()
+	}
+}
+
+func getPlaylists() {
 	config := parseConfig()
 	player := &players.Spotify{HttpClient: &http.Client{}, Config: config.Spotify}
 	player.Authenticate()
@@ -31,8 +50,18 @@ func main() {
 		fmt.Println("MUSICS:")
 		fmt.Printf("%v\n\n", playlist.Musics)
 	}
+}
 
-	fmt.Println("FINISH")
+func parseConfig() PlayersConfig {
+	data, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	var config PlayersConfig
+	yaml.Unmarshal(data, &config)
+
+	return config
 }
 
 func fetchMusics(player players.Player, playlist *players.Playlist, wg *sync.WaitGroup) {
